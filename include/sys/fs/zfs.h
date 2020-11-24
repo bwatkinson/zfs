9adcbfd4223430a81777032effd289807ad53632
Brian Atkinson <batkinson@lanl.gov>
2020-11-24 13:00:00 -0700

    Adding Direct IO Support
    Adding O_DIRECT support to ZFS to bypass the ARC for writes/reads.
    
    O_DIRECT support in ZFS will always ensure there is coherency between
    buffered and O_DIRECT IO requests. This ensures that all IO requests,
    whether buffered or direct, will see the same file contents at all
    times. Just as in other FS's , O_DIRECT does not imply O_SYNC. While
    data is written directly to VDEV disks, metadata will not be synced
    until the associated  TXG is synced.
    For both O_DIRECT read and write request the offset and requeset sizes,
    at a minimum, must be PAGE_SIZE aligned. In the event they are not,
    then EINVAL is returned unless the direct property is set to always (see
    below).
    
    For O_DIRECT writes:
    The request also must be block aligned (recordsize) or the write
    request will take the normal (buffered) write path. In the event that
    request is block aligned and a cached copy of the buffer in the ARC,
    then it will be discarded from the ARC forcing all further reads to
    retrieve the data from disk.
    
    For O_DIRECT reads:
    The only alignment restrictions are PAGE_SIZE alignment. In the event
    that the requested data is in buffered (in the ARC) it will just be
    copied from the ARC into the user buffer.
    
    For both O_DIRECT writes and reads the O_DIRECT flag will be ignored in
    the event that file contents are mmap'ed. In this case, all requests
    that are at least PAGE_SIZE aligned will just fall back to the buffered
    paths. If the request however is not PAGE_SIZE aligned, EINVAL will
    be returned as always regardless if the file's contents are mmap'ed.
    
    Since O_DIRECT writes go through the normal ZIO pipeline, the
    following operations are supported just as with normal buffered writes:
    Checksum
    Compression
    Dedup
    Encryption
    Erasure Coding
    There is one caveat for the data integrity of O_DIRECT writes that is
    distinct for each of the OS's supported by ZFS.
    FreeBSD - FreeBSD is able to place user pages under write protection so
              any data in the user buffers and written directly down to the
    	  VDEV disks is guaranteed to not change. There is no concern
    	  with data integrity and O_DIRECT writes.
    Linux - Linux is not able to place anonymous user pages under write
            protection. Because of this, if the user decides to manipulate
    	the page contents while the write operation is occurring, data
    	integrity can not be guaranteed. However, there is a module
    	parameter `zfs_vdev_direct_write_verify_pct` that contols the
    	percentage of O_DIRECT writes that can occur to a top-level
    	VDEV before a checksum verify is run before the contents of the
    	user buffers are committed to disk. In the event of a checksum
    	verification failure the write will be redirected through the
    	ARC. The deafault value for `zfs_vdev_direct_write_verify_pct`
    	is 2 percent of Direct I/O writes to a top-level VDEV. The
    	number of O_DIRECT write checksum verification errors can be
    	observed by doing `zpool status -d`, which will list all
    	verification errors that have occurred on a top-level VDEV.
    	Along with `zpool status`, a ZED event will be issues as
    	`dio_verify` when a checksum verification error occurs.
    
    A new dataset property `direct` has been added with the following 3
    allowable values:
    disabled - Accepts O_DIRECT flag, but silently ignores it and treats
    	   the request as a buffered IO request.
    standard - Follows the alignment restrictions  outlined above for
    	   write/read IO requests when the O_DIRECT flag is used.
    always   - Treats every write/read IO request as though it passed
               O_DIRECT and will do O_DIRECT if the alignment restrictions
    	   are met otherwise will redirect through the ARC. This
    	   property will not allow a request to fail.
    
    Signed-off-by: Brian Atkinson <batkinson@lanl.gov>
    Co-authored-by: Mark Maybee <mark.maybee@delphix.com>
    Co-authored-by: Matt Macy <mmacy@FreeBSD.org>
    Co-authored-by: Brian Behlendorf <behlendorf@llnl.gov>

--- include/sys/fs/zfs.h
+++ include/sys/fs/zfs.h
@@ -191,6 +191,7 @@ typedef enum {
 	ZFS_PROP_REDACTED,
 	ZFS_PROP_REDACT_SNAPS,
 	ZFS_PROP_SNAPSHOTS_CHANGED,
+	ZFS_PROP_DIRECT,
 	ZFS_NUM_PROPS
 } zfs_prop_t;
 
@@ -514,6 +515,12 @@ typedef enum {
 	ZFS_VOLMODE_DEV = 2,
 	ZFS_VOLMODE_NONE = 3
 } zfs_volmode_t;
+
+typedef enum {
+	ZFS_DIRECT_DISABLED = 0,
+	ZFS_DIRECT_STANDARD,
+	ZFS_DIRECT_ALWAYS
+} zfs_direct_t;
 
 typedef enum zfs_keystatus {
 	ZFS_KEYSTATUS_NONE = 0,
