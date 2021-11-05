8abaa18ba5026f8f398b48083658c22deae9f1ac
Brian Atkinson <batkinson@lanl.gov>
2021-11-05 09:55:52 -0600

    Reverted zfs_setup_direct() to previous version
    Finally found the mistake I made with zfs_setup_direct(). In the case
    of os->os_direct == ZFS _DIRECT_DISABLED I was not calling
    ZFS_EXIT(zfsvfs) before returning EAGAIN. This of course led to
    deadlocks. However, there still is the issue of Direct IO reads with
    FreeBSD returning EFAULT on occasions because the user pages were not
    mapped. This still has to be fully investigated and fixed.
    
    Signed-off-by: Brian Atkinson <batkinson@lanl.gov>

--- module/os/freebsd/zfs/zfs_vnops_os.c
+++ module/os/freebsd/zfs/zfs_vnops_os.c
@@ -4433,7 +4433,10 @@ zfs_freebsd_read(struct vop_read_args *ap)
 #endif
 		}
 
-		return (error);
+		if (error != EAGAIN)
+			return (error);
+		else
+			ioflag &= ~O_DIRECT;
 	}
 
 	error = zfs_read(zp, &uio, ioflag, ap->a_cred);
@@ -4465,7 +4468,10 @@ zfs_freebsd_write(struct vop_write_args *ap)
 	error = zfs_setup_direct(zp, &uio, UIO_WRITE, &ioflag);
 
 	if (error) {
-		return (error);
+		if (error != EAGAIN)
+			return (error);
+		else
+			ioflag &= ~O_DIRECT;
 	}
 
 	error = zfs_write(zp, &uio, ioflag, ap->a_cred);
