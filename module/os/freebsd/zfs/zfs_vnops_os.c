fc61d6ca5cdfad88a07606b2c79cee73035ec8c2
Brian Atkinson <batkinson@lanl.gov>
2021-11-04 22:35:30 -0600

    Had to update how zfs_setup_direct() is used
    Until the issue with EFAULT being returned from FreeBSD is resolved I
    had to change how we use zfs_setup_direct(). It no longer returns
    EAGAIN. This means that Linux has to check the flags for O_DIRECT after
    calling zfs_setup_direct() to determine if the it should still do
    O_DIRECT without an error returning from zfs_setup_direct().
    
    Using this update, all the direct tests pass in FreeBSD. This is mostly
    just to mask the issue with EFAULT at the moment and this issue really
    must be fixed on the FreeBSD side.
    
    Signed-off-by: Brian Atkinson <batkinson@lanl.gov>

--- module/os/freebsd/zfs/zfs_vnops_os.c
+++ module/os/freebsd/zfs/zfs_vnops_os.c
@@ -4413,9 +4413,7 @@ zfs_freebsd_read(struct vop_read_args *ap)
 #endif
 		}
 
-		if (error != EAGAIN)
-			return (error);
-		ioflag &= ~O_DIRECT;
+		return (error);
 	}
 
 	error = zfs_read(zp, &uio, ioflag, ap->a_cred);
@@ -4447,9 +4445,7 @@ zfs_freebsd_write(struct vop_write_args *ap)
 	error = zfs_setup_direct(zp, &uio, UIO_WRITE, &ioflag);
 
 	if (error) {
-		if (error !=  EAGAIN)
-			return (error);
-		ioflag &= ~O_DIRECT;
+		return (error);
 	}
 
 	error = zfs_write(zp, &uio, ioflag, ap->a_cred);
